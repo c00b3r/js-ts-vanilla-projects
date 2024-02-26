87a7cc5ad2ccc585832930d9bae2d47d5dd9343d
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-26 16:40:03 +0500

    feat: add interface and type function on loader.ts and some var on other .ts file

--- news-js-main/src/components/app/app.ts
+++ news-js-main/src/components/app/app.ts
@@ -2,12 +2,14 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
+    start(): void {
         document
             .querySelector('.sources')
             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
