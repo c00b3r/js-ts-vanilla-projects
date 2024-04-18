f0e81925aa69c30d90d437024003bbc522e35f44
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-04-19 01:00:55 +0500

    fix: tag lefend dont show and some refactor code for readable

--- fun-chat/src/components/view/AuthorizationForm/AuthorizationForm.ts
+++ fun-chat/src/components/view/AuthorizationForm/AuthorizationForm.ts
@@ -2,27 +2,6 @@ import "./AuthorizationForm.css";
 
 const form = document.createElement("form");
 const fieldset = document.createElement("fieldset");
-fieldset.classList.add("input-wrapper");
-
-const createFormBody = () => {
-  form.classList.add("form");
-
-  const legend = document.createElement("legend");
-  legend.textContent = "Авторизация";
-  fieldset.appendChild(legend);
-  form.append(fieldset);
-
-  const buttonAuthorization = document.createElement("button");
-  buttonAuthorization.textContent = "Войти";
-  buttonAuthorization.classList.add("button-authorization");
-  const buttonInfo = document.createElement("button");
-  buttonInfo.textContent = "Информация";
-  buttonInfo.classList.add("button-info");
-  form.appendChild(buttonAuthorization);
-  form.appendChild(buttonInfo);
-
-  document.body.append(form);
-};
 
 const createInputUser = () => {
   const inputUserLogin = `
@@ -38,12 +17,38 @@ const createInputUser = () => {
   <input class="input" type="password" placeholder="Введите пароль">
   </div>
  `;
+
   fieldset.innerHTML = inputUserLogin + inputUserPassword;
+};
+
+const createFormBody = () => {
+  form.classList.add("form");
+  fieldset.classList.add("input-wrapper");
+
+  createInputUser();
+
+  const legend = document.createElement("legend");
+  legend.textContent = "Авторизация";
+  fieldset.appendChild(legend);
+
+  form.append(fieldset);
+
+  const buttonAuthorization = document.createElement("button");
+  buttonAuthorization.textContent = "Войти";
+  buttonAuthorization.classList.add("button-authorization");
+
+  const buttonInfo = document.createElement("button");
+  buttonInfo.textContent = "Информация";
+  buttonInfo.classList.add("button-info");
+
+  form.appendChild(buttonAuthorization);
+  form.appendChild(buttonInfo);
+
+  document.body.append(form);
 };
 
 const AuthorizationForm = () => {
   createFormBody();
-  createInputUser();
 };
 
 AuthorizationForm();
